bc9618439778bf768b559d1bdf18dbb461b3a6b9
Awadhesh Kumar <awadhesh0043@gmail.com>
2024-02-09 17:22:06 +0530

    model files and html updated

--- app.py
+++ app.py
@@ -1,5 +1,4 @@
 # Import python libraries
-import uvicorn
 from fastapi import FastAPI, Request
 from fastapi.responses import HTMLResponse
 from fastapi.templating import Jinja2Templates
@@ -7,67 +6,86 @@ from fastapi.staticfiles import StaticFiles
 from flask import Flask
 from constants import TOP_N_RECOMMENDATIONS
 import model as model
-from data_model import *
+from data_model import InputDataModel, OutputDataModel, UserDataModel
 
 # Init fastAPI
 app = FastAPI()
 # Init flask APP
 flask_app = Flask(__name__)
 
-def apply_model(username, limit):
-	return model.product_recommendations(username, limit)
 
-# API Predict recommendations	
+def apply_model(username, limit):
+    return model.product_recommendations(username, limit)
+
+
+# API Predict recommendations
 @app.post("/api/predict", response_model=OutputDataModel)
 async def api_post_predictions(inputDataModel: InputDataModel):
-    ''' Default limit is 5 if provided Zero or Negative'''
-    if (inputDataModel.limit <= 0):
+    """Default limit is 5"""
+    if inputDataModel.limit <= 0:
         recommendations = apply_model(inputDataModel.username, TOP_N_RECOMMENDATIONS)
     else:
-        recommendations = apply_model(inputDataModel.username, inputDataModel.limit)  
-    if (recommendations is not None):
+        recommendations = apply_model(inputDataModel.username, inputDataModel.limit)
+
+    if recommendations is not None:
         recommendations = list(recommendations.values.tolist())
-        response = {"message": f"Top {len(recommendations)} recommendations for User [{inputDataModel.username}].",
-                    "recommendations": recommendations}
+        response = {
+            "message": f"Top {len(recommendations)} recommendations for User [{inputDataModel.username}].",
+            "recommendations": recommendations,
+        }
     else:
         response = {"message": f"User [{inputDataModel.username}] not found. Please try again!"}
     return response
+
 
 # API List Users
 @app.get("/api/users", response_model=UserDataModel)
 async def api_get_users():
     return {"users": model.get_users()}
 
+
 # FastAPI Templating
 
 # Static files mount
-app.mount("/static", StaticFiles(directory='templates/css'), name="static")
+app.mount("/static", StaticFiles(directory="templates/css"), name="static")
 # App templates for web pages
-deafult_templates = Jinja2Templates(directory='templates')
-app_templates = Jinja2Templates(directory='templates/app/')
+deafult_templates = Jinja2Templates(directory="templates")
+app_templates = Jinja2Templates(directory="templates/app/")
+
 
 # Loading Index Html
-@app.get('/')
-async def index_html(request:Request):
-    return deafult_templates.TemplateResponse("index.html", {'request': request})
+@app.get("/")
+async def index_html(request: Request):
+    return deafult_templates.TemplateResponse("index.html", {"request": request})
+
 
 # Loading Recommendation page
-@app.get('/app', response_class=HTMLResponse)
-async def index_html(request:Request):
-	return app_templates.TemplateResponse('recommendations.html', {'request': request}) 
+@app.get("/app", response_class=HTMLResponse)
+async def recommendation_html(request: Request):
+    return app_templates.TemplateResponse("recommendations.html", {"request": request})
+
 
 # Recommendation page with predictions
 @app.get("/app/ml_predict", response_class=HTMLResponse)
 async def app_post_predictions(request: Request, username: str):
-    username=username.strip().lower()
-    if (username != ''):
+    username = username.strip().lower()
+    if username != "":
         recommendations = apply_model(username, TOP_N_RECOMMENDATIONS)
-        if (recommendations is not None):
-            column_names=recommendations.columns.values
+        if recommendations is not None:
+            column_names = recommendations.columns.values
             recommendations = list(recommendations.values.tolist())
-            return app_templates.TemplateResponse('recommendations.html', {'request': request, 'recommendations': recommendations, 'column_names': column_names, 'user_input': username, 'zip': zip})
+            return app_templates.TemplateResponse(
+                "recommendations.html",
+                {
+                    "request": request,
+                    "recommendations": recommendations,
+                    "column_names": column_names,
+                    "user_input": username,
+                    "zip": zip,
+                },
+            )
         else:
-            message=f"User [{username}] not found. Please try again!"
+            message = f"User [{username}] not found. Please try again!"
     else:
-        message="Username shouldn't be Empty, Whitespaces!"
-    return app_templates.TemplateResponse('recommendations.html', {'request': request, 'err_msg': message})
+        message = "Username shouldn't be Empty, Whitespaces!"
+    return app_templates.TemplateResponse("recommendations.html", {"request": request, "err_msg": message})
--- data_model.py
+++ data_model.py
@@ -1,12 +1,15 @@
 from pydantic import BaseModel
 
+
 class InputDataModel(BaseModel):
     username: str
     limit: int
-	
+
+
 class OutputDataModel(BaseModel):
     message: str
     recommendations: list = []
+
 
 class UserDataModel(BaseModel):
     users: list = []
